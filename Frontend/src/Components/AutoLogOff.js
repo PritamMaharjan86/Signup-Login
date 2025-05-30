17cac8bbd1fc1457c21da0aeff9ee53eb3f3a043
Pritam Maharjan <primgdev@gmail.com>
2025-05-30 21:21:08 +1000

    adding auto log off

---
+++ Frontend/src/Components/AutoLogOff.js
@@ -0,0 +1,35 @@
+import { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const AUTO_LOGOUT_TIME = 1 * 60 * 100;
+
+export default function AutoLogoutOff() {
+    const navigate = useNavigate();
+    const timeoutRef = useRef(null);
+
+    const logout = () => {
+        localStorage.removeItem('token'); 
+        localStorage.removeItem('loggedIn');
+        navigate('/login');
+    };
+
+    const resetTimer = () => {
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
+        timeoutRef.current = setTimeout(logout, AUTO_LOGOUT_TIME);
+    };
+
+    useEffect(() => {
+        window.addEventListener('mousemove', resetTimer);
+        window.addEventListener('keydown', resetTimer);
+
+        resetTimer();
+
+        return () => {
+            window.removeEventListener('mousemove', resetTimer);
+            window.removeEventListener('keydown', resetTimer);
+            clearTimeout(timeoutRef.current);
+        };
+    }, []);
+
+    return null;
+}
--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
+import AutoLogoutOff from '../Components/AutoLogOff';
 
 function Home() {
     const [loggedIn, setLoggedIn] = useState('');
@@ -8,12 +9,22 @@ function Home() {
 
     useEffect(() => {
         setLoggedIn(localStorage.getItem('loggedIn'));
+
+        const handleTabClose = () => {
+            localStorage.removeItem('token');
+            localStorage.removeItem('loggedIn');
+            toast.success("Logged out!")
+        };
+        window.addEventListener('beforeunload', handleTabClose);
+        return () => {
+            window.removeEventListener('beforeunload', handleTabClose);
+        };
     }, []);
 
     const handleLogout = (e) => {
         localStorage.removeItem('token');
         localStorage.removeItem('loggedIn');
-        toast.success("Logging out!");
+        toast.success("Logging out!")
         setTimeout(() => {
             navigate('/login')
         }, 1000);
@@ -26,9 +37,8 @@ function Home() {
                 <h1 class="text-4xl font-bold text-gray-800 mb-4">
                     Welcome, {loggedIn}
                 </h1>
-
+                <AutoLogoutOff />
                 <ToastContainer />
-
                 <button
                     onClick={handleLogout}
                     class="mt-4 px-6 py-2 bg-gradient-to-r from-green-500 to-blue-500 text-white font-semibold rounded-lg shadow-lg hover:from-green-600 hover:to-blue-600 transition-all duration-300">
@@ -40,3 +50,4 @@ function Home() {
 }
 
 export default Home
+;
