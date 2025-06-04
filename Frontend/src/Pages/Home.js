6c194aabd97f15a9a98de5be87c7dc0d120ca991
Pritam Maharjan <primgdev@gmail.com>
2025-06-04 19:35:33 +1000

    update home UI

--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -33,15 +33,15 @@ function Home() {
 
     return (
         <div>
-            <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-                <h1 class="text-4xl font-bold text-gray-800 mb-4">
+            <div class="flex flex-col items-start justify-start min-h-screen bg-gray-100 m-5">
+                <h1 class="text-xl font-bold text-gray-800 mb-4">
                     Welcome, {loggedIn}
                 </h1>
                 <AutoLogoutOff />
                 <ToastContainer />
                 <button
                     onClick={handleLogout}
-                    class="mt-4 px-6 py-2 bg-gradient-to-r from-green-500 to-blue-500 text-white font-semibold rounded-lg shadow-lg hover:from-green-600 hover:to-blue-600 transition-all duration-300">
+                    class="px-2 bg-gradient-to-r from-green-500 to-blue-500 text-white font-semibold rounded-lg shadow-lg hover:from-green-600 hover:to-blue-600 transition-all duration-300">
                     Logout
                 </button>
             </div>
